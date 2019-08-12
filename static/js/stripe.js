145baaafc505f8b48ea7bc785393d4e5a7e0f489
Ubuntu <ubuntu@ip-172-31-82-56.ec2.internal>
2019-08-12 21:29:18 +0000

    did not save stripe.js file so was not commited in the previous commit

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,4 +1,3 @@
-
 $(function() {
     $("#payment-form").submit(function() {
         var form = this;
@@ -9,13 +8,15 @@ $(function() {
             cvc: $("#id_cvv").val()
         };
     
+    
+    
     Stripe.createToken(card, function(status, response) {
         if (status === 200) {
             $("#credit-card-errors").hide();
             $("#id_stripe_id").val(response.id);
 
-            // Prevent the credit card details from being submitted
-            // to our server
+            // Prevent card details from being uploaded to our server
+            
             $("#id_credit_card_number").removeAttr('name');
             $("#id_cvv").removeAttr('name');
             $("#id_expiry_month").removeAttr('name');
@@ -27,7 +28,11 @@ $(function() {
             $("#credit-card-errors").show();
             $("#validate_card_btn").attr("disabled", false);
         }
+        
     });
+    
     return false;
+    
     });
+    
 });
