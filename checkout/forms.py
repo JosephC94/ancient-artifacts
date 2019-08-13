19e7fdcf0ad972bb46fe6d2f937c9dfd40635f98
Ubuntu <ubuntu@ip-172-31-82-56.ec2.internal>
2019-08-13 14:57:10 +0000

    no major changes made - stripe payments not working so currently looking at the issueby making some very minor changes to code. using AWS and not educate - all code being pushed to github and AWS account being deleted so the Educate account can be set up and used instead as instructed by Code Institute

--- checkout/forms.py
+++ checkout/forms.py
@@ -9,7 +9,7 @@ class MakePaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
     YEAR_CHOICES = [(i, i) for i in range(2019, 2050)]
 
-    credit_card_number = forms.CharField(label='Card number', required=False)
+    credit_card_number = forms.CharField(label='Card number', required=False, min_length=15, max_length=16)
     cvv = forms.CharField(label='CVV code', required=False)
     expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
